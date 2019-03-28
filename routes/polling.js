437ec61c4627b416a0e0b02e445d705cbaf6fa4f
Montero14 <adrmonte@ucm.es>
2019-03-28 19:22:25 +0100

    PollingRemoteFile correcto

--- routes/polling.js
+++ routes/polling.js
@@ -53,124 +53,32 @@ function pollingRemoteFile(req, res, next)
  	let url = req.body.urlName;
  	const fn = url.split('/');
  	let fileName = fn[fn.length-1];
- 	let contentFile
+ 	let contentFile, originalContentFile
+
+	console.log(`Visualizando cambios en el fichero ` + fileName);
 
 	request.get(url, function (error, response, body) {
 	    if (!error && response.statusCode == 200) {
+	        originalContentFile = body;
+	    	}
+		});
+
+
+	function pollingFile()
+	{
+		request.get(url, function (error, response, body) {
+	    if (!error && response.statusCode == 200) {
+	    	//console.log("Realizando comprobacion")
 	        contentFile = body;
-	    }
-	});
-
-	require('log-timestamp');
-	console.log(`Visualizando cambios en el fichero ` + fileName);
-
-/*
-
-	// Import the watching library
-	let watchr = require('watchr')
-	 
-	// Define our watching parameters
-	let path = url;
-	function listener (changeType, fullPath, currentStat, previousStat) {
-	    switch ( changeType ) {
-	        case 'update':
-	            console.log('the file', fullPath, 'was updated', currentStat, previousStat)
-	            break
-	        case 'create':
-	            console.log('the file', fullPath, 'was created', currentStat)
-	            break
-	        case 'delete':
-	            console.log('the file', fullPath, 'was deleted', previousStat)
-	            break
-	    }
+	        if (contentFile != originalContentFile)
+		        {
+		        	//console.log("Fichero " + fileName + "cambiado.")
+		        	originalContentFile = contentFile;
+		        }
+	    	}
+		});
 	}
-	function next (err) {
-	    if ( err )  return console.log('watch failed on', path, 'with error', err)
-	    console.log('watch successful on', path)
-	}
-	 
-	// Watch the path with the change listener and completion callback
-//	let stalker = watchr.open(path, listener, next)
-	 
-	// Close the stalker of the watcher
-//	stalker.close()
-
-	// Create the stalker for the path
-	stalker = watchr.create(path)
-	 
-	// Listen to the events for the stalker/watcher
-	// http://rawgit.com/bevry/watchr/master/docs/index.html#watcher
-	stalker.on('change', listener)
-	stalker.on('log', console.log)
-	stalker.once('close', function (reason) {
-	    console.log('closed', path, 'because', reason)
-	    stalker.removeAllListeners()  // as it is closed, no need for our change or log listeners any more
-	})
-	 
-	// Set the default configuration for the stalker/watcher
-	// http://rawgit.com/bevry/watchr/master/docs/index.html#Watcher%23setConfig
-	stalker.setConfig({
-	    stat: null,
-	    interval: 5007,
-	    persistent: true,
-	    catchupDelay: 2000,
-	    preferredMethods: ['watch', 'watchFile'],
-	    followLinks: true,
-	    ignorePaths: false,
-	    ignoreHiddenFiles: false,
-	    ignoreCommonPatterns: true,
-	    ignoreCustomPatterns: null
-	})
-	 
-	// Start watching
-	stalker.watch(next)
-	 
-	// Stop watching
-	stalker.close()
-*/
-/*
-	let http = require('http');
-	let options = {method: 'HEAD', host: 'http://147.96.96.25', port: 3000, path: '/public/PruebaJSON'};
-	let req = http.request(options, function(res) {
-	    console.log(res.headers);
-	  }
-	);
-	req.end();
-*/
-/*
-	fs.watchFile(url, (curr, prev) => {
-	  console.log(fileName + ' ha cambiado');
-	});
-*/
- /*
- function(url,callback){
-    request(url).on('data',(data) => {
-        try{
-            var json = JSON.parse(data);    
-        }
-        catch(error){
-            callback("");
-        }
-        callback(json);
-    })
-}
- */
- /*
-  	let fileContent;
-  	let fs = require('fs');
- 	let filePath = '/../resources/file.json';
- 	let fileName = 'file.json';
-
-	fileContent = fs.readFileSync(__dirname + filePath, 'utf-8');
-	let json = JSON.parse(fileContent); 
-
-	require('log-timestamp');
-	console.log(`Visualizando cambios en el fichero ${fileName}`);
-
-	fs.watchFile(__dirname + filePath, (curr, prev) => {
-	  console.log(`${fileName} ha cambiado`);
-	});
-*/
+	setInterval(pollingFile,1000);
 }
 
 
