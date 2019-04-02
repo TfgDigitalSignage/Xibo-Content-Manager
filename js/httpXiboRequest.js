2175343e8a909a45ff26252fefc1f1ab1c29bef7
DanielGutierrezDelgado <danielgtic0910@gmail.com>
2019-04-02 18:11:13 +0200

    parametrización de variables

--- js/httpXiboRequest.js
+++ js/httpXiboRequest.js
@@ -3,17 +3,17 @@ var request = require ('request');
 var qs = require('querystring');
 var constant = {
     //CLIENT INFO GABRI
-    client_id: '6Ca5RWWPXR6Jq1kpYV9BV7z0nP6G9WAx7yacSnhs',
-    client_secret: '5GJrGW6RGMHeTkowBSTdsypdnWJaXG1uVWf9NnCPru74UOSjvlTMAk6FH5wi1eMm5rpzZbIuhV39oMoM1SQYD7f64FSQzMX2fZaQv3hcc8bYpncfplQhTy7fHIVWojUWvbd0BXmEXiZsagmyxRCcCy3Xcv8DHD2q2N6aNK1H1HfrWwTnZGTMkTkJMupaRP6L0Z1vrmeMCJ4sTmIb8srZeDRNAgYAId2r9uE2fnwvWeUYWJNonUB0exs2iQhWYj',
-    baseUrl: 'http://localhost/api/'
+    //client_id: '6Ca5RWWPXR6Jq1kpYV9BV7z0nP6G9WAx7yacSnhs',
+    //client_secret: '5GJrGW6RGMHeTkowBSTdsypdnWJaXG1uVWf9NnCPru74UOSjvlTMAk6FH5wi1eMm5rpzZbIuhV39oMoM1SQYD7f64FSQzMX2fZaQv3hcc8bYpncfplQhTy7fHIVWojUWvbd0BXmEXiZsagmyxRCcCy3Xcv8DHD2q2N6aNK1H1HfrWwTnZGTMkTkJMupaRP6L0Z1vrmeMCJ4sTmIb8srZeDRNAgYAId2r9uE2fnwvWeUYWJNonUB0exs2iQhWYj',
+    //baseUrl: 'http://localhost/api/'
     //CLIENT INFO ADRI
     //client_id: 'Tv7PRCwXCi3n6po1WcuhXzIsVZv0gb0gXE6kl7if',
     // client_secret: 'ndTS6bNNxQ4S9qnT8akJrEoeOBHCO4RLmAYcrNlTMCPIxjfCL5Oc9HeEUv6Oi8Bq0OXQ2LQKCBUh7DUvqOKLY3L1aLyi1ngwcgByFi5YQ0nYfiJeOspmsFbBruT0GitdIp4AsFyRoMdytjVgqXiUvxQ20VtJ7iHmdozO4Hj5pO1F0lMWX9WBocBCoIrBBFS51P7sqjxu2QIh8ywOeEG3MhqvqIyTYod45NCopwpYfwJxWSp5kGswvweHWWZKIr',
     // baseUrl: 'http://localhost/api/'
     //CLIENT INFO DANI
-    // client_id: 'Aw8RNRb5AEqmS7B8C5ipq5XcV40LxagxnD41sCmg',
-    // client_secret: '6gWsZef5ajJiTKmuPiQB56vCrlVQi86o0DqxTiKZyzu1XpzX4jzSug5BPRmnTFbjLDgcVVXTFsO0594mp1e07qAgvMxAjiEt1Yo83bYy4G6YgUD0EPKDJPGzIdhqUhc8iD7WyExfj9oDLauG2R4n0um5cMUEPVNI3ZvOOkJPoTXsV8K6xmA25Jscif3ZOncUQ5ivCfordmIlg0C5IHTVIjWGn9EyXGNECLsIZLBGAKwka3Eq01MqRKpPnR9u7F',
-    //baseUrl: 'http://localhost/api/'
+     client_id: 'Aw8RNRb5AEqmS7B8C5ipq5XcV40LxagxnD41sCmg',
+     client_secret: '6gWsZef5ajJiTKmuPiQB56vCrlVQi86o0DqxTiKZyzu1XpzX4jzSug5BPRmnTFbjLDgcVVXTFsO0594mp1e07qAgvMxAjiEt1Yo83bYy4G6YgUD0EPKDJPGzIdhqUhc8iD7WyExfj9oDLauG2R4n0um5cMUEPVNI3ZvOOkJPoTXsV8K6xmA25Jscif3ZOncUQ5ivCfordmIlg0C5IHTVIjWGn9EyXGNECLsIZLBGAKwka3Eq01MqRKpPnR9u7F',
+    baseUrl: 'http://localhost/api/'
 }
 
 function getAccessToken (callback){
@@ -139,7 +139,7 @@ function getLibraryMedia(token, callback){
   });
 }
 
-function postSchedule(token, layoutId, displayGroupIds, fromDt, toDt, callback) {
+function postSchedule(token, layoutId, displayGroupIds, fromDt, toDt,priority,priority, callback) {
   const options = {
     url: constant.baseUrl + 'schedule',
     headers: 
@@ -152,7 +152,7 @@ function postSchedule(token, layoutId, displayGroupIds, fromDt, toDt, callback) 
       eventTypeId: '1',
       campaignId: layoutId,
       displayOrder: '1',
-      isPriority: '0',
+      isPriority: priority,
       'displayGroupIds[]': displayGroupIds,
       fromDt: fromDt,
       toDt: toDt
@@ -167,7 +167,7 @@ request.post(options, (error, response, body) => {
 
 }
 
-function putSchedule(token, idLayout,idEvent,displayGroupIds,fromDt, toDt, callback){
+function putSchedule(token, idLayout,idEvent,displayGroupIds,fromDt, toDt,priority, callback){
   var options = {
     url: constant.baseUrl + "schedule/"+ idEvent,
     headers: {
@@ -178,7 +178,7 @@ function putSchedule(token, idLayout,idEvent,displayGroupIds,fromDt, toDt, callb
       eventTypeId: 1,
       campaignId: idLayout,
       displayOrder: 1,
-      isPriority: 0,
+      isPriority: priority,
       'displayGroupIds[]': displayGroupIds,
       fromDt: fromDt,
       toDt: toDt
