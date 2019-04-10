605af521422834aedf8b2889f1c64afbcbf7daa2
Montero14 <adrmonte@ucm.es>
2019-04-10 20:50:38 +0200

    Avanzando DesignLayout

--- js/httpXiboRequest.js
+++ js/httpXiboRequest.js
@@ -7,13 +7,13 @@ var constant = {
     //client_secret: '5GJrGW6RGMHeTkowBSTdsypdnWJaXG1uVWf9NnCPru74UOSjvlTMAk6FH5wi1eMm5rpzZbIuhV39oMoM1SQYD7f64FSQzMX2fZaQv3hcc8bYpncfplQhTy7fHIVWojUWvbd0BXmEXiZsagmyxRCcCy3Xcv8DHD2q2N6aNK1H1HfrWwTnZGTMkTkJMupaRP6L0Z1vrmeMCJ4sTmIb8srZeDRNAgYAId2r9uE2fnwvWeUYWJNonUB0exs2iQhWYj',
     //baseUrl: 'http://localhost/api/'
     //CLIENT INFO ADRI
-    //client_id: 'Tv7PRCwXCi3n6po1WcuhXzIsVZv0gb0gXE6kl7if',
-    //client_secret: 'ndTS6bNNxQ4S9qnT8akJrEoeOBHCO4RLmAYcrNlTMCPIxjfCL5Oc9HeEUv6Oi8Bq0OXQ2LQKCBUh7DUvqOKLY3L1aLyi1ngwcgByFi5YQ0nYfiJeOspmsFbBruT0GitdIp4AsFyRoMdytjVgqXiUvxQ20VtJ7iHmdozO4Hj5pO1F0lMWX9WBocBCoIrBBFS51P7sqjxu2QIh8ywOeEG3MhqvqIyTYod45NCopwpYfwJxWSp5kGswvweHWWZKIr',
-    //baseUrl: 'http://localhost/api/'
-    //CLIENT INFO DANI
-    client_id: 'Aw8RNRb5AEqmS7B8C5ipq5XcV40LxagxnD41sCmg',
-    client_secret: '6gWsZef5ajJiTKmuPiQB56vCrlVQi86o0DqxTiKZyzu1XpzX4jzSug5BPRmnTFbjLDgcVVXTFsO0594mp1e07qAgvMxAjiEt1Yo83bYy4G6YgUD0EPKDJPGzIdhqUhc8iD7WyExfj9oDLauG2R4n0um5cMUEPVNI3ZvOOkJPoTXsV8K6xmA25Jscif3ZOncUQ5ivCfordmIlg0C5IHTVIjWGn9EyXGNECLsIZLBGAKwka3Eq01MqRKpPnR9u7F',
+    client_id: 'Tv7PRCwXCi3n6po1WcuhXzIsVZv0gb0gXE6kl7if',
+    client_secret: 'ndTS6bNNxQ4S9qnT8akJrEoeOBHCO4RLmAYcrNlTMCPIxjfCL5Oc9HeEUv6Oi8Bq0OXQ2LQKCBUh7DUvqOKLY3L1aLyi1ngwcgByFi5YQ0nYfiJeOspmsFbBruT0GitdIp4AsFyRoMdytjVgqXiUvxQ20VtJ7iHmdozO4Hj5pO1F0lMWX9WBocBCoIrBBFS51P7sqjxu2QIh8ywOeEG3MhqvqIyTYod45NCopwpYfwJxWSp5kGswvweHWWZKIr',
     baseUrl: 'http://localhost/api/'
+    //CLIENT INFO DANI
+    //client_id: 'Aw8RNRb5AEqmS7B8C5ipq5XcV40LxagxnD41sCmg',
+    //client_secret: '6gWsZef5ajJiTKmuPiQB56vCrlVQi86o0DqxTiKZyzu1XpzX4jzSug5BPRmnTFbjLDgcVVXTFsO0594mp1e07qAgvMxAjiEt1Yo83bYy4G6YgUD0EPKDJPGzIdhqUhc8iD7WyExfj9oDLauG2R4n0um5cMUEPVNI3ZvOOkJPoTXsV8K6xmA25Jscif3ZOncUQ5ivCfordmIlg0C5IHTVIjWGn9EyXGNECLsIZLBGAKwka3Eq01MqRKpPnR9u7F',
+    //baseUrl: 'http://localhost/api/'
 }
 
 function getAccessToken (callback){
@@ -112,6 +112,22 @@ function deleteLayout(token, idLayout, callback){
       throw new Error(err);
     callback && callback (response);
   });
+}
+
+function getWidgetsOfPlaylist(playlistId, token, callback){
+  var options = {
+    url: constant.baseUrl + "playlist/widget?playlistId=" + playlistId,
+    headers:{
+    'content-type' : 'application/x-www-form-urlencoded',
+    'Authorization': 'Bearer ' + token
+    }
+  };
+
+  request.get(options, function(err, response, data){
+    if (err)
+      throw new Error(err);
+    callback && callback (response);
+    });
 }
 
 function postWidgetWebContent (playlistId, token, uri, modeId, useDuration, duration, callback){
@@ -308,6 +324,7 @@ exports.getJsonData = getJsonData
 exports.getLayout = getLayout
 exports.postLayout = postLayout
 exports.deleteLayout = deleteLayout
+exports.getWidgetsOfPlaylist = getWidgetsOfPlaylist
 exports.postHlsWidget = postHlsWidget
 exports.postWidgetWebContent = postWidgetWebContent
 exports.postOrderWidget = postOrderWidget
--- routes/layoutManager.js
+++ routes/layoutManager.js
@@ -7,15 +7,17 @@ let params = {
     layoutName : "",
     layoutId : "",
     layoutBackgroundColor : "",
-    layoutBackgroundzIndex : ""
+    layoutBackgroundzIndex : "",
+    layoutRegion: "",
+    layoutPlaylist:""
 }
 
 
 router.get('/LayoutManager', (req,res,next) => {
     layoutController.getLayout(params, (layouts)=>{
-            //console.log(layouts);
-            res.render('layoutManager.pug', {
-                layouts: layouts});
+        //console.log(layouts);
+        res.render('layoutManager.pug', {
+            layouts: layouts});
         });
 });
 
@@ -31,6 +33,21 @@ router.post('/deleteLayout', (req,res,next) => {
     layoutController.deleteLayout(params, ()=>{
         });
     res.redirect('/');
+});
+
+router.post('/designLayout', (req,res,next) =>{
+    selectedLayout = JSON.parse(req.body.layoutChecked)
+    params.layoutId = selectedLayout.layoutId
+    params.layoutName = selectedLayout.layouts
+    params.layoutBackgroundColor = selectedLayout.layoutBackgroundColor
+    params.layoutBackgroundzIndex = selectedLayout.layoutBackgroundzIndex
+    params.layoutRegion = selectedLayout.regions[0]
+    params.layoutPlaylist = selectedLayout.regions[0].playlists[0]
+    layoutController.getWidgets(params, (widgets)=>{
+        res.render('designLayout.pug', {
+            widgets: widgets,
+            length: widgets.length});
+        });
 });
 
 
