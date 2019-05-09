7fdbc907b918178ce500081e01617f47c1914e11
Gabriel Gutierrez <gagutier@ucm.es>
2019-05-09 11:19:16 +0200

    Bug fix en el orden de la clasificacion

--- controller/CompetitionController.js
+++ controller/CompetitionController.js
@@ -8,25 +8,43 @@ module.exports = {
     },
 
     updateScoreboard: (contestId, response) => {
+        let cont = 0;
         domJudgeServices.getScoreboard(contestId, board => {
             const teams = []
             const rows = JSON.parse(board).rows;
-            for (const row in rows){
+            /* for (const row in rows){
                 domJudgeServices.getOneTeamFromContest(contestId, rows[row].team_id, team => {
-                    team = JSON.parse(team) 
-                    teams.push({
-                        'rank': rows[row].rank, 
-                        'name': team.name, 
+                    const rank = rows[row].rank
+                    teams[rank] = {
+                        'rank': rank, 
+                        'name': JSON.parse(team).name, 
                         'score': rows[row].score.num_solved, 
                         'total': rows[row].score.total_time
-                    })
-                    if (teams.length == rows.length){
-                        response.render('scoreboard.pug', {
+                    }
+                    if (cont == rows.length){
+                        response.status(200).render('scoreboard', {
                             'teams': teams
                         });
                     }
                 })
-            }
+            } */
+            rows.forEach((element,index) => {
+                domJudgeServices.getOneTeamFromContest(contestId, element.team_id, team => {
+                    teams[index] = {
+                        'rank': element.rank, 
+                        'name': JSON.parse(team).name, 
+                        'score': element.score.num_solved, 
+                        'total': element.score.total_time
+                    }
+                    cont++;
+                    if (cont == rows.length){
+                        console.log(teams)
+                        response.status(200).render('scoreboard', {
+                            'teams': teams
+                        });
+                    }
+                })
+            });
         })
     }
 }
