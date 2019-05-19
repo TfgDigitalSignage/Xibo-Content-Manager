16de396020935d2818ef84c81e4b66d626b94bdf
Gabriel Gutierrez <gagutier@ucm.es>
2019-05-19 20:08:11 +0200

    Refactoring and deleting unused dependencies/files

--- app.js
+++ app.js
@@ -9,9 +9,7 @@ app.set('view engine', 'pug');
 app.set('views', 'view');
 
 const index_route = require('./routes/index');
-const remoteContent_route = require('./routes/remoteDataLoader'); 
 const scheduleLayout_route = require('./routes/eventManager');
-const polling_route = require('./routes/polling');
 const competition_route = require('./routes/competition')
 const layoutManager_route = require('./routes/layoutManager')
 const campaignManager_route = require('./routes/campaignManager')
@@ -22,9 +20,7 @@ app.use(body_parser.urlencoded({extended: true}));
 app.use(express.static('public'))
 
 app.use(index_route);
-app.use(remoteContent_route);
 app.use(scheduleLayout_route);
-app.use(polling_route);
 app.use('/competition', competition_route)
 app.use(layoutManager_route)
 app.use(campaignManager_route)
